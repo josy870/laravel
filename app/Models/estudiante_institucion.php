cbeb0aae7de33e7a929b41af400410ce08cd62d4
josy870 <yucramamanijoselynmilagros870@gmail.com>
2025-05-05 23:34:07 -0500

    app

---
+++ app/Models/estudiante_institucion.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class estudiante_institucion extends Model
+{
+    use HasFactory;
+}
